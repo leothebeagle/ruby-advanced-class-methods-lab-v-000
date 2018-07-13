71433b8f6bfe6171ebf8db458170295450763801
Aziz Kosber <a.kosber@gmail.com>
2018-07-13 02:49:13 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,7 +41,7 @@ class Song
     artist_name = parts[0]
     song_name = parts[1].gsub(".mp3", "")
     song = self.new_by_name(song_name)
-    song.artist = artist_name
+    song.artist_name = artist_name
     song
   end
 
