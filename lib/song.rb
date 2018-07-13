60d42ab2515b9bb3cb1998c721ef96ad57b8af01
Aziz Kosber <a.kosber@gmail.com>
2018-07-13 02:39:23 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,7 +29,7 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    self.create_by_name unless self.find_by_name == true
+    self.create_by_name(name) unless self.find_by_name(name) == true
   end
 
   def save #instance method. an instance is able to add itself to the class roster that keeps track of all songs created.
