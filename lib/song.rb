a55762d601c76ceac3c6b5020937f86e97523fb4
Aziz Kosber <a.kosber@gmail.com>
2018-07-13 01:30:04 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,14 +1,9 @@
-require 'pry'
 class Song
-  attr_accessor :name, :artist_name
   @@all = []
+  attr_accessor :name, :artist_name
 
   def self.all
     @@all
-  end
-
-  def save
-    self.class.all << self
   end
 
   def self.create
@@ -17,44 +12,12 @@ class Song
     song
   end
 
-  def self.new_by_name(name)
-    song = self.new
-    song.name = name
-    song
+  def self.new_by_name
+
   end
 
-  def self.create_by_name(name)
-    song = self.create
-    song.name = name
-    song
+  def save #instance method. an instance is able to add itself to the class roster that keeps track of all songs created. 
+    self.class.all << self
   end
 
-  def self.find_by_name(name)
-    self.all.find {|song| song.name == name}
-  end
-
-  def self.find_or_create_by_name(name)
-    self.find_by_name(name) || self.create_by_name(name)
-  end
-
-  def self.alphabetical
-    self.all.sort_by { |song| song.name}
-  end
-
-  def self.new_from_filename(filename)
-    song_data = filename.split(' - ')
-    name = song_data[1].chomp(".mp3")
-    artist = song_data[0]
-    song = self.new_by_name(name)
-    song.artist_name = artist
-    song
-  end
-
-  def self.create_from_filename(filename)
-    self.new_from_filename(filename).save
-  end
-
-  def self.destroy_all
-    self.all.clear
-  end
 end
